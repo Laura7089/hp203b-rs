0573bc05029fceea2d03a7d8300bcaa276889f02
Laura Demkowicz-Duffy <laura@laurademkowiczduffy.co.uk>
2022-09-20 17:33:04 +0100

    tests: restructure self-tests and add a couple

--- self-tests/tests/basic.rs
+++ self-tests/tests/basic.rs
@@ -11,52 +11,90 @@ use panic_probe as _;
 #[used]
 pub static BOOT_LOADER: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
 
-use fugit::RateExtU32;
-use hal::{gpio, i2c, pac};
-use rp2040_hal as hal;
-
-type I2CPin<P> = gpio::Pin<P, gpio::Function<gpio::I2C>>;
-type I2C = i2c::I2C<pac::I2C0, (I2CPin<gpio::bank0::Gpio16>, I2CPin<gpio::bank0::Gpio17>)>;
-
-fn setup() -> I2C {
-    let mut perips = pac::Peripherals::take().unwrap();
-    let sio = hal::Sio::new(perips.SIO);
-    let pins = hal::gpio::Pins::new(
-        perips.IO_BANK0,
-        perips.PADS_BANK0,
-        sio.gpio_bank0,
-        &mut perips.RESETS,
-    );
-
-    hal::I2C::i2c0(
-        perips.I2C0,
-        pins.gpio16.into_mode(),
-        pins.gpio17.into_mode(),
-        400.kHz(),
-        &mut perips.RESETS,
-        125_000_000.Hz(),
-    )
-}
-
 #[defmt_test::tests]
 mod tests {
-    use super::{setup, I2C};
-    use defmt::assert;
-    #[allow(unused_imports)]
-    use hp203b::interrupts::HasInterrupts;
     use hp203b::{Channel, OSR};
 
-    type HP203B<M> = hp203b::HP203B<I2C, M, hp203b::csb::CSBLow>;
+    use defmt::info;
+    use fugit::RateExtU32;
+    use hal::{gpio, i2c, pac};
+    use rp2040_hal as hal;
+    use shared_bus::BusManagerSimple;
 
-    #[test]
-    fn always_passes() {
-        assert!(true);
+    type I2CPin<P> = gpio::Pin<P, gpio::Function<gpio::I2C>>;
+    type I2C = i2c::I2C<pac::I2C0, (I2CPin<gpio::bank0::Gpio16>, I2CPin<gpio::bank0::Gpio17>)>;
+
+    type HP203B<I, M> = hp203b::HP203B<I, M, hp203b::csb::CSBHigh>;
+
+    #[init]
+    fn get_i2c() -> BusManagerSimple<I2C> {
+        let mut perips = pac::Peripherals::take().unwrap();
+        let sio = hal::Sio::new(perips.SIO);
+        let pins = hal::gpio::Pins::new(
+            perips.IO_BANK0,
+            perips.PADS_BANK0,
+            sio.gpio_bank0,
+            &mut perips.RESETS,
+        );
+
+        BusManagerSimple::new(hal::I2C::i2c0(
+            perips.I2C0,
+            pins.gpio16.into_mode(),
+            pins.gpio17.into_mode(),
+            400.kHz(),
+            &mut perips.RESETS,
+            125_000_000.Hz(),
+        ))
     }
 
     #[test]
-    fn make_new() {
-        let _: HP203B<hp203b::mode::Pressure> =
-            HP203B::new(setup(), OSR::OSR128, Channel::SensorPressureTemperature)
-                .expect("HP203B::new returned an error");
+    fn make_new(i2c: &mut BusManagerSimple<I2C>) {
+        HP203B::new(
+            i2c.acquire_i2c(),
+            OSR::OSR1024,
+            Channel::SensorPressureTemperature,
+        )
+        .unwrap();
+    }
+
+    #[test]
+    fn is_ready(i2c: &mut BusManagerSimple<I2C>) {
+        let mut alti = HP203B::new(
+            i2c.acquire_i2c(),
+            OSR::OSR1024,
+            Channel::SensorPressureTemperature,
+        )
+        .unwrap();
+        alti.is_ready().unwrap();
+    }
+
+    #[test]
+    fn read_pressure(i2c: &mut BusManagerSimple<I2C>) {
+        let mut alti = HP203B::new(
+            i2c.acquire_i2c(),
+            OSR::OSR1024,
+            Channel::SensorPressureTemperature,
+        )
+        .unwrap();
+        info!(
+            "Pressure reading: {}Pa",
+            nb::block!(alti.read_pres()).unwrap()
+        );
+    }
+
+    #[test]
+    fn read_alti(i2c: &mut BusManagerSimple<I2C>) {
+        let mut alti = HP203B::new(
+            i2c.acquire_i2c(),
+            OSR::OSR1024,
+            Channel::SensorPressureTemperature,
+        )
+        .unwrap()
+        .to_altitude()
+        .unwrap();
+        info!(
+            "Pressure reading: {}Pa",
+            nb::block!(alti.read_alti()).unwrap()
+        );
     }
 }
